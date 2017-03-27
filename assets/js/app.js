61943f21e4ed54d03d5005fba977fdfee04b399c
AlexanderLizunov <eric_zp_ua@bigmir.net>
2017-03-27 21:08:08 +0200

    1

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by stimpik on 26.03.2017.
+ */
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcclxuICogQ3JlYXRlZCBieSBzdGltcGlrIG9uIDI2LjAzLjIwMTcuXHJcbiAqL1xyXG4iXX0=
